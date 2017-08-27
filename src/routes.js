3abfd97ae71e802a63128341a6893581376564c1
Julie Nisbet <jmnisbet@gmail.com>
2017-08-27 16:38:11 -0700

    super basic react-router

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 /**
  * Import all page components here
  */
 import App from './App';
-import NearbyPage from './components/NearbyPage';
+import NearbyList from './components/NearbyList';
 import SelectedProfile from './components/SelectedProfile';
 
 /**
  * All routes go here.
  * Don't forget to import the components above after adding new route.
  */
-export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={NearbyPage} />
-    <Route path="/profile" component={SelectedProfile} />
-  </Route>
-);
+export default class AppRouter extends React.Component{
+	render(){
+  	  return(	
+  	  	<BrowserRouter>
+    	  	<Route path="/" component={App}>
+    	    	<Route exact path="/" component={NearbyList} />
+    	    	<Route path="/profile" component={SelectedProfile} />
+    	  	</Route>
+    	  </BrowserRouter>);
+	};
+}
