a39191fb82ebc339dd00921e43dc713e62d7d607
richardverheyen <richard.verheyen@gmail.com>
2017-09-01 15:42:24 -0400

    Reorder all stylesheets into sass and import variables and fonts

--- src/components/SelectedProfile.js
+++ src/components/SelectedProfile.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 const SelectedProfile = ({selectedProfile}) =>
   <div id="selected-profile">
-    <Link to={`/`}><img src="/img/arrow-right.svg"/></Link>
+    <Link to={`/`}><img src="/img/arrow-right.svg" alt=""/></Link>
     <img src={selectedProfile.data.attributes.image} alt="nearby profile introduc"/>
     <p>{selectedProfile.data.attributes.tagline}</p>
   </div>
