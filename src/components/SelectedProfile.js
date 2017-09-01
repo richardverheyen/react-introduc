c3b36c214b4549970879135726dae195414dd825
richardverheyen <richard.verheyen@gmail.com>
2017-09-01 13:28:26 -0400

    Add a return buttont to the selectedProfile page

--- src/components/SelectedProfile.js
+++ src/components/SelectedProfile.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom'
+
 
 const SelectedProfile = ({selectedProfile}) =>
-  <div>
+  <div id="selected-profile">
+    <Link to={`/`}><img src="/img/arrow-right.svg"/></Link>
     <img src={selectedProfile.data.attributes.image} alt="nearby profile introduc"/>
     <p>{selectedProfile.data.attributes.tagline}</p>
   </div>
