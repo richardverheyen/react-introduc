895ee0ea6c4397b8a22be6c9b27de11bb1749a9c
richardverheyen <richard.verheyen@gmail.com>
2017-09-05 15:44:21 -0700

    Set the state of CustomiseUser component to the input values

--- src/components/CustomiseUser.js
+++ src/components/CustomiseUser.js
@@ -2,14 +2,53 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class CustomiseUser extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      image: 'img/ewatson.jpg',
+      tagline: 'hello'
+    };
+    this.imageUpdated = this.imageUpdated.bind(this);
+    this.taglineUpdated = this.taglineUpdated.bind(this);
+  }
+
+  imageUpdated(e) {
+    const { value } = e.target;
+    this.setState({ image: value });
+  }
+  taglineUpdated(e) {
+    const { value } = e.target;
+    this.setState({ tagline: value });
+  }
+
   render() {
     return (
     <section id="customise-user">
+      <ul>
+        <li>
+          <p>upload your image here:</p>
+          <input
+            type="input"
+            onChange={this.imageUpdated}
+            name="image"
+            value={this.state.image}
+          />
+        </li>
+        <li>
+          <p>upload your tagline here:</p>
+          <input
+            type="input"
+            onChange={this.taglineUpdated}
+            name="tagline"
+            value={this.state.tagline}
+          />
+        </li>
+        <button className='post-button'>Post up to the server</button>
+      </ul>
       <Link to={`/`} className="return"><img src="/img/arrow-down-big.svg" alt=""/></Link>
-        <form>
-          <h1>Form</h1>
-        </form>
-      </section>
+    </section>
     )
   }
 }
